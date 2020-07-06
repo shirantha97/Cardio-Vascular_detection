ae26a7eeb2dd6a1b73c9d9d6be161713d087531f
shirantha97 <39570216+shirantha97@users.noreply.github.com>
2020-07-07 01:06:51 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request, redirect
+import cloudpickle
+import json
+import numpy as np
+
+app = Flask(__name__, template_folder='templates')
+model = cloudpickle.load(open('finalModel.sav', 'rb'))
+
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+
+@app.route('/forms')
+def forms():
+    return render_template('Predict.html')
+
+
+@app.route("/predict", methods=['POST', 'GET'])
+def do_post_search():
+    # BMI = request.form.get('BMI')
+    # age = request.form.get('AgeInput')
+    # # sysBp = request.form.get('')
+    int_features = [float(x) for x in request.form.values()]
+    final = [np.array(int_features)]
+    print(final)
+    return render_template('home.html')
+
+
+if __name__ == "_main_":
+    app.run(debug=True)
