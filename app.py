137d636e150785eaeb13f741e72651f5b8b0c63d
shirantha97 <39570216+shirantha97@users.noreply.github.com>
2020-07-13 09:55:06 +0530

    update requirments.txt

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 import cloudpickle
 import json
+from json import JSONEncoder
 import numpy as np
 
 app = Flask(__name__,static_url_path='', static_folder='static', template_folder='templates')
@@ -24,8 +25,14 @@ def do_post_search():
     print(final)
     predict = model.predict(final)
     prediction_prob = model.predict_proba(final)
-    print(prediction_prob[1])
-    return render_template('home.html')
+    print(prediction_prob)
+    output = {
+        "positive prediction": (prediction_prob[0][1]*100).item(),
+        "negative prediction": (prediction_prob[0][0]*100).item()
+    }
+    json_output = json.dumps(output)
+    return json_output
+    # return render_template('home.html')
 
 
 if __name__ == "_main_":
