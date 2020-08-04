4b03aab14e0cd9fd59dc86473ce7476af2e54325
shirantha97 <39570216+shirantha97@users.noreply.github.com>
2020-08-05 01:38:22 +0530

    Handle post requests

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_cors import CORS
 import cloudpickle
 import json
 from json import JSONEncoder
 import numpy as np
 
-app = Flask(__name__,static_url_path='', static_folder='static', template_folder='templates')
+app = Flask(__name__, static_url_path='', static_folder='static', template_folder='templates')
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+# cors = CORS(app, resources={
+#     r"/": {
+#         "origins": "*"
+#     }
+# })
 model = cloudpickle.load(open('finalModel.sav', 'rb'))
 
 
@@ -20,32 +28,39 @@ def forms():
 
 @app.route("/predict", methods=['POST', 'GET'])
 def do_post_search():
-    bmi = request.args.get('BMI')
-    diabetes = request.args.get('diabetes')
-    diaBp = request.args.get('diaBp')
-    sysBp = request.args.get('sysBp')
-    totChol = request.args.get('totChol')
-    glucose = request.args.get('glucose')
-    bpmeds = request.args.get('bpmeds')
-    stroke = request.args.get('stroke')
-    hypertension = request.args.get('hypertension')
-    cigs = request.args.get('cigs')
-    age = request.args.get('age')
-    sex = request.args.get('sex')
+    data = request.data
+    datadict = json.loads(data)
+    bmi = float(datadict["bmi"])
+    diabetes = float(datadict["diabetes"])
+    diaBp = float(datadict["diaBp"])
+    sysBp = float(datadict["sysBp"])
+    totChol = float(datadict["totChol"])
+    glucose = float(datadict["glu"])
+    bpmeds = float(datadict["bpmeds"])
+    stroke = float(datadict["stroke"])
+    hypertension = float(datadict["hypertension"])
+    cigs = float(datadict["cigs"])
+    age = float(datadict["age"])
+    sex = float(datadict["sex"])
 
     int_features = [bmi, diabetes, diaBp, sysBp, totChol, glucose, bpmeds, stroke, hypertension, cigs, age, sex]
     final = [np.array(int_features)]
     print(final)
     predict = model.predict(final)
     prediction_prob = model.predict_proba(final)
-    print(prediction_prob)
+    print(predict)
     output = {
         "positive prediction": (prediction_prob[0][1]*100).item(),
         "negative prediction": (prediction_prob[0][0]*100).item()
     }
     json_output = json.dumps(output)
     return json_output
-    # return render_template('home.html')
+    # output = {
+    #     "positive prediction": "gg",
+    #     "negative prediction": "d"
+    # }
+    # json_output = json.dumps(output)
+    # return json_output
 
 
 if __name__ == "_main_":
